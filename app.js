530322ddd81f43df0843f3a2896f9aac56b5584e
nicolas blum <nblumferracci@gmail.com>
2021-02-02 13:25:04 +0100

    reusable components with props and nested components

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-Vue.component('click-counter', {
-  template: '<button @click="count++">{{count}}</button>',
-  data () {
+// Vue.component('click-counter', {
+//   template: '#click-counter-template',
+//   data () {
+//     return {
+//       count: 0
+//     }
+//   }
+// }),
+
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
     return {
-      count: 0
+      plans: ['The hacker', 'The curious', 'The Addict', 'The Single']
+    }
+  }
+})
+Vue.component('plan', {
+  template: '#plan-template',
+  props:{
+    name: {
+      type: String,
+      required: true
     }
   }
 })
 
 new Vue({
-  el: '#app'
+  el: '#app',
 })
