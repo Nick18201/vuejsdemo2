45a39d26c9793d879b819b6ccb6b6991069bb85b
nicolas blum <nblumferracci@gmail.com>
2021-02-02 14:47:11 +0100

    coomunication between components and custom events

--- app.js
+++ app.js
@@ -7,8 +7,8 @@
 //   }
 // }),
 
-let planComponent = {
-  template: '#plan-template',
+let PlanPickerItemComponent = {
+  template: '#plan-picker-item-template',
   props:{
     name: {
       type: String,
@@ -33,7 +33,7 @@ let planComponent = {
 let PlanPickerComponent = {
   template: '#plan-picker-template',
   components: {
-    plan: planComponent
+    'plan-picker-item': PlanPickerItemComponent
   },
   data() {
     return {
