45f449ed55b0d2f7c8a3bdd7a1b156aff6ad0c00
nicolas blum <nblumferracci@gmail.com>
2021-02-02 14:27:04 +0100

    coomunication between components and custom events

--- app.js
+++ app.js
@@ -7,24 +7,50 @@
 //   }
 // }),
 
-Vue.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The hacker', 'The curious', 'The Addict', 'The Single']
-    }
-  }
-})
-Vue.component('plan', {
+let planComponent = {
   template: '#plan-template',
   props:{
     name: {
       type: String,
       required: true
+    },
+    selectedPlan: {
+      type: String
+    }
+  },
+  computed: {
+    isSelected() {
+      return this.name === this.selectedPlan
+    }
+  },
+  methods: {
+    select() {
+      this.$emit('select', this.name)
     }
   }
-})
+}
+
+let PlanPickerComponent = {
+  template: '#plan-picker-template',
+  components: {
+    plan: planComponent
+  },
+  data() {
+    return {
+      plans: ['The hacker', 'The curious', 'The Addict', 'The Single'],
+      selectedPlan: null
+      }
+  },
+  methods: {
+    selectPlan(plan) {
+      this.selectedPlan = plan
+    }
+  }
+}
 
 new Vue({
   el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent
+  }
 })
